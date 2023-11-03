d8af0d7b8bbbc999d4b4b3a7e6274e4a4377798e
tif <youfoundtiffany@gmail.com>
2023-11-03 13:00:38 -0600

    wip

--- RenuSolutions.client/src/pages/HomePage.vue
+++ RenuSolutions.client/src/pages/HomePage.vue
@@ -1,8 +1,9 @@
 <!-- HOME PAGE -->
 <template>
+  <!-- 🖼️ TOPLINE OUTER WRAPPER W/ BACKGROUND IMAGE -->
   <section class="container-flex text-center top-sec-bg-image justify-content-center m-0 p-0">
 
-    <!-- NAVBAR ON HOME PAGE ONLY -->
+    <!-- 🧭 NAVBAR ON HOME PAGE ONLY -->
     <section class="m-0 p-0 d-flex align-items-center justify-content-center">
       <nav v-if="$route.name === 'Home'"
         class="navbar bg-transparent navbar-expand-sm navbar-light navbar-expand-md pe-2 m-0">
@@ -49,14 +50,12 @@
         </div>
       </nav>
     </section>
-    <!-- HOME PAGE NAVBAR ONLY -->
-    <!-- HOME PAGE CONTENT -->
-    <section>
-      <div class="container border border-warning">
-        <div
-          class="border border-danger row justify-content-around text-Midnight p-0 m-0 animate__animated animate__backInLeft">
+    <!-- 🧭 HOME PAGE NAVBAR ONLY END-->
+    <section class="" style="min-height: 35vh;">
+      <div class="container ">
+        <div class=" row justify-content-around text-Midnight p-0 m-0 animate__animated animate__backInLeft">
           <div class="row">
-            <div class="col-8">
+            <div class="col-12 col-md-6 col-sm-4">
               <h1 class=" fw-bold text-start ms-5 mt-3 mb-0 ps-5 big-txt-msg text-light">Custom Web
                 Solutions<br>for the
                 Modern Small Business
@@ -70,15 +69,15 @@
                 designs<br>that ensure superior performance, with plans starting at $150 a month."
               </p>
             </div>
-            <div class="col-4 justify-content-start d-flex">
+            <div class="col-12 col-md-4 justify-content-start d-flex">
               <!-- FIXME Add media query -->
-              <img class="ad-img justify-content-start" src="src/assets/img/mock-up-replace.png" alt=""
-                style="max-width:;">
+              <img class="ad-img justify-content-start" src="src/assets/img/mock-up-replace.png" alt="">
             </div>
-            <div class="container d-flex , ms-5 ps-5">
-              <div class="container d-flex ps-5" style="margin-top: -15vh; margin-left: 15vw;">
+            <!-- FIXME THIS NEEDS A MEDIA QUERY -->
+            <div class="container d-flex ms-5 ps-5">
+              <div class="container d-flex ps-5 ps-md-5" style="margin-top: -5vh; margin-left: 15vw;">
                 <router-link :to="{ name: 'Contact' }"
-                  class="smol-txt-btn btn lighten-30 text-uppercase fw-bold text-Midnight rounded bg-Minty align-items-center d-flex">
+                  class="smol-txt-btn btn lighten-30 text-uppercase fw-bold text-Midnight rounded bg-Minty align-items-center d-flex elevation-3">
                   GET IN TOUCH!
                 </router-link>
               </div>
@@ -86,42 +85,137 @@
           </div>
         </div>
       </div>
-      <!-- <div style="height: 40vh;"> -->
-      <!-- spacer div -->
-      <!-- </div> -->
-
     </section>
-    <!-- HOME PAGE CONTENT END -->
-    <!-- BODY -->
-    <div class="container mt-3">
-      <div class="row justify-content-center align-items-center g-2">
-        <div class="col"></div>
-
-      </div>
-    </div>
-    <!-- 3 CARDS -->
-    <div class="container mt-3">
-      <div class="row justify-content-center align-items-center g-2">
-        <div class="col">Column</div>
-        <div class="col">Column</div>
-        <div class="col">Column</div>
-      </div>
-    </div>
-    <!-- 3 CARDS -->
-
-    <!-- BODY END -->
+    <!-- 🧭 NAVBAR END -->
   </section>
+  <!-- 🖼️ TOPLINE OUTER WRAPPER W/ BACKGROUND IMAGE END-->
+
+
+
+  <!-- 🚀 spacer div -->
+  <!-- <div style="height: 60vh;">
+  </div> -->
+  <!-- 🚀 spacer div -->
+
+
+  <!-- HOME PAGE CONTENT -->
+  <!--🦄 BODY -->
+
+  <!-- 1️⃣SINGLE CONTENT DIV -->
+  <div class="container my-5 text-Midnight">
+    <div class="row py-5 justify-content-center align-items-center">
+      <div class="col-12 col-md-6 text-center">
+        <h1 class="fw-bold">WHAT WE DO</h1>
+        <div class="text-Midnight">
+          <span>———</span><i class="mdi
+          mdi-pyramid"></i><span>———</span>
+        </div>
+        <p>We Specialize in providing a quality, full-stack, business system that has a proven track record of increasing
+          revenue for small businesses like yours. This allows you the time and freedome to meet with your clients, which
+          brings in more revenue.
+        </p>
+        <p class="fst-italic">You finally get to focus on what you really love about your business!
+        </p>
+        <div class="text-Midnight">
+          <span>———</span><i class="mdi
+          mdi-pyramid mdi-flip-v"></i><span>———</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- SINGLE CONTENT DIV -->
+
+
+  <!-- 2️⃣ TRIPLE CARDS -->
+  <section class="container my-3">
+    <div class="row justify-content-around">
+      <!-- 🅰️ -->
+      <div class="col-3 card h-30 elevation-3 text-Midnight">
+        <i class="mdi mdi-pipe fs-1 onhvr text-center"></i>
+        <div class="card-body">
+          <h5 class="card-title  text-center">Full-Stack</h5>
+          <p class="card-text text-start Merri-light-smol "> We build a system that has every important tool at your
+            fingertips,
+            and they all
+            work
+            together!</p>
+        </div>
+      </div>
+      <!-- 🅱️ -->
+      <div class="col-3 card h-30 elevation-3 text-Midnight">
+        <i class="mdi mdi-tablet-cellphone fs-1 onhvr text-center"></i>
+        <div class="card-body">
+          <h5 class="card-title text-center">Mobile First</h5>
+          <p class="card-text text-start Merri-light-smol">Your web products will display and be repsponsive in all
+            mobile sizes.
+          </p>
+        </div>
+      </div>
+      <!-- 🆎 -->
+      <div class="col-3 card h-30 elevation-3 text-Midnight">
+        <i class="mdi mdi-key fs-1 onhvr text-center"></i>
+        <div class="card-body">
+          <h5 class="card-title text-center">Turn Key</h5>
+          <p class="text-start Merri-light-smol">
+            Everything you need to
+            run your entire
+            business is all organized for you.</p>
+        </div>
+      </div>
+    </div>
+    <!-- 2️⃣ TRIPLE CARDS END-->
+
+    <!-- 3️⃣ Horizontal Card DIV -->
+    <section class="container my-5 text-Midnight">
+      <div class="row py-5 justify-content-center align-items-center">
+        <div class="col-3">
+          <img src="src/assets/img/piggyBank.svg" alt="" class="piggyBank">
+        </div>
+        <div class="col-12 col-md-6 text-center">
+          <h1 class="fw-bold">$150 per Month,<br>6 month Minimum Contract</h1>
+
+          <p><span><i class="mdi mdi-checkbox-marked-circle-outline"></i></span>We Specialize in providing a quality,
+            full-stack
+          </p>
+
+
+        </div>
+      </div>
+    </section>
+
+
+    <!--🦄 BODY END -->
+  </section>
+
+
+  <!-- </section> -->
 </template>
 
 <script>
 export default {
   setup() {
+    // document.getElementById('animateMe').addEventListener('mouseenter', function () {
+    //   this.classList.add('animate__bounce');
+    // });
+
+    // document.getElementById('animateMe').addEventListener('mouseleave', function () {
+    //   this.classList.remove('animate__bounce');
+    // });
+
     return {}
   }
 }
 </script>
 
 <style scoped lang="scss">
+.piggyBank {
+  max-height: 10rem;
+  max-width: 100%;
+  object-fit: cover;
+  object-position: center;
+}
+
+
 // large and mobile ad image
 // @media screen and (max-width: 601px) {
 //   .mobile-ad-image {
@@ -136,14 +230,29 @@ export default {
 //   }
 // }
 
-// big and small text for topline message
+// END big and small text for topline message
+
+// letter spacing
+
+
+.card:hover i.onhvr {
+  scale: 1.5;
+  transition: 0.2s;
+}
+
+.Merri-light-smol {
+  font-family: 'Merriweather', serif !important;
+  font-weight: 400 !important;
+  font-size: small !important;
+}
+
 .smol-txt-msg {
-  font-size: small;
-  font-weight: 800;
+  font-size: small !important;
+  font-weight: 800 !important;
 }
 
 .big-txt-msg {
-  font-size: xx-large;
+  font-size: xx-large !important;
   // font-weight: 800;
 }
 
@@ -165,10 +274,9 @@ export default {
 
 //ad mock up image
 .ad-img {
-  height: 75vh;
-  width: 75vh;
-  object-fit: contain;
-  object-position: center;
+  max-height: 60vh;
+  object-fit: cover;
+  object-position: left center;
 }
 
 /* To Display Navbar over HomePage background image ONLY */
@@ -176,11 +284,10 @@ export default {
   background-image: url("../assets/img/final-basicbackgroundRenuSol.svg");
   // background-attachment: fixed;
   /* This keeps the background image fixed during scroll */
-  height: 140vh;
+  // height: 140vh;
   width: 100%;
   background-size: cover;
   background-position: center;
-  // height: 100vh;
   /* You can adjust this to fit the size you want */
   // position: relative;
   // z-index: 1;
@@ -188,3 +295,8 @@ export default {
 
 /* To Display Navbar over HomePage background image ONLY */
 </style>
+
+<!-- <i class="mdi mdi-pyramid"></i>
+<i class="mdi mdi-hours-24"></i>
+<i class="mdi mdi-infinity"></i>
+<i class="mdi mdi-power-plug"></i> -->
