c62e037eba978a2dd0413a7af3b1dd4d7c7291d6
tif <youfoundtiffany@gmail.com>
2023-11-08 15:25:45 -0700

    updated

--- RenuSolutions.client/src/env.js
+++ RenuSolutions.client/src/env.js
@@ -1,6 +1,9 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-5f8lqpo8uazfphqk.us.auth0.com'
+// NOTE may need to adjust the http or https depending on your server
+// NOTE the port should match the port your server is running on
+// NOTE that https is in c#
+export const audience = 'https://renusolutions.tech'
+export const clientId = 'ZxjOGSuFAFiFSnMzwP93fswfWluxkRQY'
