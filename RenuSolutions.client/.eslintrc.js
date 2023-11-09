ec2ec9dd3cae68ea8e643e52197b7de805413405
tif <youfoundtiffany@gmail.com>
2023-11-08 17:15:18 -0700

    vite.config and eslintrc.js

--- RenuSolutions.client/.eslintrc.js
+++ RenuSolutions.client/.eslintrc.js
@@ -1,7 +1,12 @@
 /* eslint-env node */
 module.exports = {
   root: true,
-
+  // NOTE
+  env: {
+    browser: true, // NOTE Set to false if you're not using this in a browser environment
+    es2021: true,
+    node: true, // NOTE This line informs ESLint about the Node.js globals
+  },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended'
