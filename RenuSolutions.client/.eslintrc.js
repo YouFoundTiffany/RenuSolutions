85bc9161c9ee1025666dc5d37b7b4272ff2f204d
tif <youfoundtiffany@gmail.com>
2023-11-08 17:16:26 -0700

    vite.config and eslintrc.js

--- RenuSolutions.client/.eslintrc.js
+++ RenuSolutions.client/.eslintrc.js
@@ -1,7 +1,12 @@
 /* eslint-env node */
 module.exports = {
   root: true,
-
+  // NOTE
+  env: {
+    browser: true, // NOTE Set to false if you're not using this in a browser environment
+    es2021: true,
+    node: true, // NOTE This line informs ESLint about the Node.js globals
+  },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended'
@@ -20,3 +25,4 @@ module.exports = {
     'vue/multi-word-component-names': 'off'
   }
 }
+
--- RenuSolutions.client/vite.config.js
+++ RenuSolutions.client/vite.config.js
@@ -1,9 +1,17 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    // NOTE added alias
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    },
+    // NOTE added alias
+  },
   build: {
     outDir: 'docs',
     sourcemap: false
