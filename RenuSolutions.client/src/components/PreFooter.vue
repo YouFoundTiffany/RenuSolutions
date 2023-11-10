a91bc036ee7945c7f4dad4cbd9350d2990ecdd94
tif <youfoundtiffany@gmail.com>
2023-11-10 11:43:31 -0700

    footer

--- RenuSolutions.client/src/components/PreFooter.vue
+++ RenuSolutions.client/src/components/PreFooter.vue
@@ -2,35 +2,70 @@
     <section class='container-flex gradient-midnight-maritime-aquatic my-2 p-0 SideEye'>
         <div class='row justify-content-around pt-3 text-center'>
             <div class="col-12 col-md-3">
-                <img :src="renuLogo" alt="Renu Logo" class=""
-                    style="object-fit: cover; object-position: center; max-height: 30vh;">
-                <p></p>
+                <router-link :to="{ name: 'Home' }" class="">
+                    <img :src="renuLogo" alt="Renu Logo" class="p-3"
+                        style="object-fit: cover; object-position: center; max-height: 25vh;">
+                </router-link>
                 <p>Email Info@RenuSolutions.tech</p>
                 <p>Call Text (208) 918-0942</p>
             </div>
-            <!-- <div class="col-12 col-md-2 text-center">
-                <p class="ftsTitleUnderline pt-1 pb-2 m-0 fw-bold">SERVICES</p>
-                <p class="borderLiner py-1 m-0"> Web Design</p>
-                <p class="borderLiner py-1 m-0">Website Maintenance</p>
-                <p class="borderLiner py-1 m-0">SEO Services</p>
-                <p class="borderLiner py-1 m-0">Content Creation</p>
-                <p class="borderLiner py-1 m-0">Logo Design</p>
+
+            <!-- 🚀SPACER DIV -->
+            <div class="col-3">
             </div>
-            <div class="col-12 col-md-2 text-center">
-                <p class="ftsTitleUnderline pt-1 pb-2 m-0 fw-bold">NAVIGATION</p>
-                <p class="borderLiner py-1 m-0">Home</p>
-                <p class="borderLiner py-1 m-0">About</p>
-                <p class="borderLiner py-1 m-0">Contact</p>
-                <p class="borderLiner py-1 m-0">Blog</p>
-            </div> -->
+            <!-- 🚀SPACER DIV -->
+
+
+            <!-- 🐕‍🦺 SERVICES -->
+            <!-- <section class="glass-effect m-0 p-0"> -->
+            <div class="col-12 col-md-1 text-center">
+                <!-- <p class="ftsTitleUnderlineServ pt-1 m-0 fw-bold txt-shad">SERVICES</p> -->
+                <!-- <router-link :to="{ name: 'WEBSITES' }">
+                    <p class="borderLinerServ text-light pt-1">WEBSITES</p>
+                </router-link> -->
+                <!-- <p class="borderLinerServ text-light txt-shad pt-1">WEBSITES</p>
+                <p class="borderLinerServ text-light txt-shad">WEB DESIGN</p>
+                <p class="borderLinerServ text-light txt-shad">MAINTENANCE</p>
+                <p class="borderLinerServ text-light txt-shad">SEO</p>
+                <p class="borderLinerServ text-light txt-shad">CONTENT CREATION</p>
+                <p class="borderLinerServ text-light txt-shad">LOGOS</p>
+                <p class="borderLinerServ text-light txt-shad">APPS</p> -->
+            </div>
+            <!-- 🧭 NAVIGATION -->
+            <div class="col-12 col-md-1 text-center glass-effect">
+                <p class="ftsTitleUnderlineNav pt-1 m-0 fw-bold txt-shad">NAVIGATION</p>
+                <router-link :to="{ name: 'Home' }">
+                    <p class="borderLinerNav text-light pt-1 txt-shad">HOME</p>
+                </router-link>
+                <router-link :to="{ name: 'About' }">
+                    <p class="borderLinerNav text-light txt-shad">ABOUT</p>
+                </router-link>
+                <router-link :to="{ name: 'Services' }">
+                    <p class="borderLinerNav text-light txt-shad">SERVICES</p>
+                </router-link>
+                <router-link :to="{ name: 'Portfolio' }">
+                    <p class="borderLinerNav text-light txt-shad">PORTFOLIO</p>
+                </router-link>
+                <router-link :to="{ name: 'Blog' }">
+                    <p class="borderLinerNav text-light txt-shad">BLOG</p>
+                </router-link>
+                <router-link :to="{ name: 'Contact' }">
+                    <p class="borderLinerNav text-light txt-shad">CONTACT</p>
+                </router-link>
+            </div>
+            <!-- </section> -->
+
+
+
+
         </div>
 
 
 
         <!-- bottom row -->
-        <!-- <div class='row pt-3 pb-2 m-0 text-center SideEye'>
-            <p class="privpoli mb-0 p-0">PRIVACY POLICY</p>
-        </div> -->
+        <div class='row pt-3 pb-2 m-0 text-center SideEye'>
+            <!-- <p class="privpoli mb-0 p-0">PRIVACY POLICY</p> -->
+        </div>
     </section>
 </template>
 <script>
@@ -39,7 +74,6 @@ import RenuLogo from '@/assets/img/RenuLogoMockUp.png';
 export default {
     setup() {
         return {
-
             renuLogo: RenuLogo,
         };
     },
@@ -47,27 +81,49 @@ export default {
 </script>
 
 
-<style>
+<style scoped lang="scss">
+// glass effect
+.glass-effect {
+    backdrop-filter: brightness(110%);
+}
+
+// TEXT SHADOW
+.txt-shad {
+    text-shadow: 2px 2px 1px rgba(0, 0, 0, 0.694);
+}
+
 /* TITLE STATIC UNDERLINE */
 .privpoli:hover {
     color: var(--Midnight);
     cursor: pointer;
 }
 
-.ftsTitleUnderline {
+/* UNDERLINE SERVICES */
+.ftsTitleUnderlineServ {
+    border-bottom: 1px solid var(--Aquatic);
+    font-size: small;
+}
+
+/* UNDERLINE SERVICES */
+/* UNDERLINE NAVIGATION */
+.ftsTitleUnderlineNav {
     border-bottom: 1px solid var(--Maritime);
     font-size: small;
 }
 
-/* ANIMATED UNDERLINE */
-.borderLiner {
+/* UNDERLINE NAVIGATION */
+
+/* ANIMATED UNDERLINE SERVICES */
+.borderLinerServ {
     border: none;
     position: relative;
     overflow: hidden;
-    /* Ensure the pseudo-element doesn't overflow the container */
+    text-align: center;
+    width: 90%;
+    margin-left: 4%;
 }
 
-.borderLiner::after {
+.borderLinerServ::after {
     content: '';
     position: absolute;
     width: 0;
@@ -78,7 +134,34 @@ export default {
     transition: all ease-in-out 0.2s;
 }
 
-.borderLiner:hover::after {
+.borderLinerServ:hover::after {
+    width: 100%;
+    left: 0;
+}
+
+/* ANIMATED UNDERLINE SERVICES */
+/* ANIMATED UNDERLINE NAVIGATION */
+.borderLinerNav {
+    border: none;
+    position: relative;
+    overflow: hidden;
+    text-align: center;
+    width: 75%;
+    margin-left: 12%;
+}
+
+.borderLinerNav::after {
+    content: '';
+    position: absolute;
+    width: 0;
+    height: 1px;
+    left: 50%;
+    bottom: 0;
+    background-color: white;
+    transition: all ease-in-out 0.2s;
+}
+
+.borderLinerNav:hover::after {
     width: 100%;
     left: 0;
 }
