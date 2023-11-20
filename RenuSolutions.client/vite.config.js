c7bcfe7492b5347892b239f2095eea9f219147d9
tif <youfoundtiffany@gmail.com>
2023-11-20 10:23:52 -0700

    vite handle copy file

--- RenuSolutions.client/vite.config.js
+++ RenuSolutions.client/vite.config.js
@@ -1,10 +1,21 @@
+// VITE CONFIG
+// ADDED COPY
+import copy from 'vite-plugin-copy'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    copy({
+      targets: [
+        { src: 'RenuSolutions.client/sitemap.xml', dest: 'docs' }
+      ],
+      hook: 'writeBundle'
+    })
+  ],
   resolve: {
     // NOTE added alias
     alias: {
